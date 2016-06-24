d5664ce9691f389c2744d8716f7f0e77567f846a
Nicolas <nicolas.gomes@live.fr>
2016-06-24 16:38:08 +0200

    Fini les exercices 1 à 3

---
+++ ex3.rb
@@ -0,0 +1,24 @@
+#affiche "i will now count my chickens:"
+puts "I will now count my chickens:"
+#affiche la phrase avec le resultat de l'opération parceque entre accolade
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+#affiche la phrase
+puts "Now I will count the eggs:"
+#affiche le resultat de l'operation
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
+#affiche la phrase et l'opération (pas le résultat)
+puts "Is it true that 3 + 2 < 5 - 7?"
+#affiche le resultat de l'opération
+puts 3 + 2 < 5 - 7
+#affiche la question et le résultat de la réponse
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+#affiche la phrase
+puts "Oh, that's why it's false."
+#affiche la phrase
+puts "How about some more."
+#affiche la question et le résultat booléen de la réponse 
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
